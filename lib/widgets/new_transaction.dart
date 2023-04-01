5881ac4902b980054132dbd3a330a4f73044e062
ibnu naufal <ibnunaufal5758@gmail.com>
2023-04-01 11:20:16 +0700

    hiding modal

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -2,19 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class NewTransaction extends StatelessWidget {
+class NewTransaction extends StatefulWidget {
   final Function addTrx;
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
 
   NewTransaction(this.addTrx);
+
+  @override
+  State<NewTransaction> createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final titleController = TextEditingController();
+
+  final amountController = TextEditingController();
 
   void _submitData(){
     if (titleController.text.isEmpty || double.parse(amountController.text) <= 0) {
       return;
     }
-    addTrx(titleController.text, double.parse(amountController.text));
+    widget.addTrx(titleController.text, double.parse(amountController.text));
+    Navigator.of(context).pop();
   }
+
   @override
   Widget build(BuildContext context) {
     return Card(
