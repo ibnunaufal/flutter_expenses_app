83507e23d22c41ff9a3c394cc0489eecd588d9fa
ibnu naufal <ibnunaufal5758@gmail.com>
2023-04-01 11:13:36 +0700

    fab

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import './widgets/user_transaction.dart';
+import 'package:flutter_expenses_app/widgets/new_transaction.dart';
+import 'package:flutter_expenses_app/widgets/transaction_list.dart';
+import './models/transaction.dart';
+import './widgets/transaction_list.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,14 +16,45 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transactions> _userTransaction = [
+    Transactions(
+        id: '1', title: "Sepatu baru", amount: 100000, date: DateTime.now()),
+    Transactions(
+        id: '2', title: "Kaos baru", amount: 100000, date: DateTime.now()),
+  ];
+
+  void _addNewTransaction(String title, double amount){
+    print("_addNewTransaction called");
+    final newTrx = Transactions(id: DateTime.now().toString(), title: title, amount: amount, date: DateTime.now());
+    
+    setState(() {
+      _userTransaction.add(newTrx);
+    });
+  }
+
+  void _startAddNewTrx(BuildContext ctx){
+    showModalBottomSheet(context: ctx, builder: (bCtx){
+      return GestureDetector(
+        onTap: (){},
+        behavior: HitTestBehavior.opaque,
+        child: NewTransaction(_addNewTransaction));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter Expenses App'),
         actions: [
-          IconButton(onPressed: null, icon: Icon(Icons.add))
+          IconButton(onPressed: () => _startAddNewTrx(context), icon: Icon(Icons.add))
         ],
       ),
       body: SingleChildScrollView(
@@ -32,11 +66,11 @@ class MyHomePage extends StatelessWidget {
               child: Container(width: double.infinity, child: Text('Chart')),
               elevation: 5,
             ),
-            UserTransaction()
+            TransactionList(_userTransaction)
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(onPressed: null, child: Icon(Icons.add),),
+      floatingActionButton: FloatingActionButton(onPressed: () => _startAddNewTrx(context), child: Icon(Icons.add),),
       floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
     );
   }
