e1a6650fafe62cace2444c5512fd43864a5366eb
ibnu naufal <ibnunaufal5758@gmail.com>
2023-04-06 10:39:16 +0700

    keyboard overlapping

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -46,45 +46,47 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Container(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            TextField(
-              decoration: InputDecoration(labelText: 'Title'),
-              controller: titleController,
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'Amount'),
-              controller: amountController,
-              onSubmitted: ((value) => _submitData),
-            ),
-            Container(
-              height: 70,
-              child: Row(
-                children: [
-                  Flexible(
-                    fit: FlexFit.tight, 
-                    child: Text(_selectedDate == null ? 'No Date Choosen' : 'Picked date: ${DateFormat.yMd().format(_selectedDate)}')
-                  ),
-                  Flexible(
-                      fit: FlexFit.tight,
-                      child: ElevatedButton(
-                          onPressed: _presentDatePicker,
-                          child: Text('Choose Date')))
-                ],
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Container(
+          padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom + 10, left: 10, right: 10, top: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText: 'Title'),
+                controller: titleController,
               ),
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  _submitData();
-                },
-                child: Text('Tambahkan'))
-          ],
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: 'Amount'),
+                controller: amountController,
+                onSubmitted: ((value) => _submitData),
+              ),
+              Container(
+                height: 70,
+                child: Row(
+                  children: [
+                    Flexible(
+                      fit: FlexFit.tight, 
+                      child: Text(_selectedDate == null ? 'No Date Choosen' : 'Picked date: ${DateFormat.yMd().format(_selectedDate)}')
+                    ),
+                    Flexible(
+                        fit: FlexFit.tight,
+                        child: ElevatedButton(
+                            onPressed: _presentDatePicker,
+                            child: Text('Choose Date')))
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    _submitData();
+                  },
+                  child: Text('Tambahkan'))
+            ],
+          ),
         ),
       ),
     );
